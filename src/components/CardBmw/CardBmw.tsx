bb0a93913e29e8f00a33e801c823b20618ae342d
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-18 14:59:28 +0300

    1 Created tree of custom component CardBmw

---
+++ src/components/CardBmw/CardBmw.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CardBmw.css';
+
+interface CardBmwProps {}
+
+const CardBmw: FC<CardBmwProps> = () => (
+  <div className="CardBmw">
+    CardBmw Component
+  </div>
+);
+
+export default CardBmw;
