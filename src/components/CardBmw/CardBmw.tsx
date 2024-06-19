3a48d11b4ee87583495f4283bb597bd3b0d7a955
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-19 10:19:56 +0300

    6 Created css style for main__date-text of card-bmw

--- src/components/CardBmw/CardBmw.tsx
+++ src/components/CardBmw/CardBmw.tsx
@@ -12,8 +12,19 @@ const CardBmw: FC<CardBmwProps> = () => (
       </div>
       <div></div>
     </div>
-    <div className='card-bmw_main'>
+    <div className='card-bmw__main'>
       <img src={require('./img/bmw_x3_drive.png')} alt='bmw-x3-drive'></img>
+      <div className='card-bmw__main-date'>
+        <p>BMW 320d xDrive</p>
+        <div className='card-bmw__main-date__text'>
+          <p>140 кВт (190 к.с.)</p>
+          <p>5.1 л/100км</p>
+          <p>Викиди СО2 133 0 гм/км</p>
+        </div>
+        <div>
+          <p>199 327 грн</p>    
+        </div>
+      </div>
       <button type='button' name='detail' className='card-bmw__btn'>Детальніше</button>
     </div>
   </div>
