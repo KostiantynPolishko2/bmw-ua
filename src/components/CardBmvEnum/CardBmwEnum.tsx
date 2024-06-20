5451a060d51d14c4aaa77fccd6306745aea59d29
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-20 08:58:59 +0300

    5 Done css style for paginatioin of list cars by 3 items at one click

--- src/components/CardBmvEnum/CardBmwEnum.tsx
+++ src/components/CardBmvEnum/CardBmwEnum.tsx
@@ -14,7 +14,32 @@ const CardBmwEnum = (props: AutoDataProps) => {
         arr.push(<CardBmw auto = {car}/>)
     }
 
-    return <>{arr}</>;
+    return (
+        <>
+            <nav className='card-bmw_menu'>
+                <a className='card-bmw_pagination' href="#page-1">1</a>
+                <a className='card-bmw_pagination' href="#page-2">2</a>
+                <a className='card-bmw_pagination' href="#page-3">3</a>
+            </nav>
+            <div className='card-bmw__scroll-container'>
+                <div id = 'page-1' className='card-bmw__scroll-page'>
+                    {arr[0]}
+                    {arr[1]}
+                    {arr[2]}
+                </div>
+                <div id = 'page-2' className='card-bmw__scroll-page'>
+                    {arr[3]}
+                    {arr[4]}
+                    {arr[5]}
+                </div>
+                <div id = 'page-3' className='card-bmw__scroll-page'>
+                    {arr[6]}
+                    {arr[7]}
+                    {arr[8]}
+                </div>
+            </div>
+        </>
+    );
 }
 
 CardBmwEnum.defaultProps = {
